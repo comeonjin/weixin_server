6e0d2f3099d8328965290e94dfcb0c28ef23913b
zhangjin <zhangjin@gaosiedu.com>
2018-06-05 22:35:27 +0800

    change db

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,36 @@ var APPID = 'wxd55316e683616eda'
 var APPSECRET = '8410acf63ac99272f3f841469dc94afc'
 var CODE = ''
 
+function sqltest(){
+    var mysql      = require('mysql');
+
+    var connection = mysql.createConnection({
+        host     : 'qdm130083629.my3w.com',  //121.42.100.211
+        port: 3306,
+        user     : 'qdm130083629',
+        password : 'yang1996',
+        database : 'qdm130083629_db',
+        insecureAuth: true
+      });
+
+    connection.connect();
+
+    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+    });
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    CODE = req.query.code
-    var filePath = '../public/dist/index.html'
-    var realPath = path.join(__dirname, filePath)
-    console.log(filePath)
-    res.sendFile( realPath )
+    // CODE = req.query.code
+    // console.log(filePath)
+    // var filePath = '../public/dist/index.html'
+    // var realPath = path.join(__dirname, filePath)
+    // res.sendFile( realPath )
+
+    sqltest()
 
 });
 
@@ -33,61 +55,37 @@ router.get('/request', function(req, res, next){
      * 根据code请求access_token的接口地址
      * 请求方式get
      */
-    // var access = req.session.accessToken
-    // var openid = req.session.openId
 
-    // console.log("your session is "+ access)
+    var url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+APPID+'&secret='+APPSECRET+'&code='+CODE+'&grant_type=authorization_code'
+    //根据code、appid、appsecret请求微信接口,获取用户数据
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(req.query.code)
+            body = JSON.parse(body)
+            //这里保存refresh_token
+            var access_token = body.access_token
+            var refresh_token = body.refresh_token
+            var openid = body.openid
+            req.session.accessToken = access_token;
+            req.session.openId = openid;
 
-    // var yanzheng = 'https://api.weixin.qq.com/sns/auth?access_token='+access+'&openid='+openid
-    // request(yanzheng, function(error, response, body){
-    //     if (!error && response.statusCode == 200) {
-    //         body = JSON.parse(body)
-    //         if(body.errcode ===0 && body.errmsg ==='ok'){
-    //             var userInfoUrl = 'https://api.weixin.qq.com/sns/userinfo?access_token='+access+'&openid='+openid+'&lang=zh_CN'
-    //             request(userInfoUrl, function(error, response, body){
-    //                 if (!error && response.statusCode == 200) {
-    //                     body = JSON.parse(body)
-    //                     res.send(body)
-    //                 }
-    //             })
-    //             return
-    //         }else{
-    //     console.log(req.query.code)
-                var url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+APPID+'&secret='+APPSECRET+'&code='+req.query.code+'&grant_type=authorization_code'
-                //根据code、appid、appsecret请求微信接口,获取用户数据
-                request(url, function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        console.log(req.query.code)
-                        body = JSON.parse(body)
-                        //这里保存refresh_token
-                        var access_token = body.access_token
-                        var refresh_token = body.refresh_token
-                        var openid = body.openid
-                        req.session.accessToken = access_token;
-                        req.session.openId = openid;
-
-                        //拉取用户信息(需scope为 snsapi_userinfo)
-                        /**
-                         * 请求方法
-                         * http：GET（请使用https协议） https://api.weixin.qq.com/sns/userinfo?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN
-                         */
-                        var userInfoUrl = 'https://api.weixin.qq.com/sns/userinfo?access_token='+access_token+'&openid='+openid+'&lang=zh_CN'
-                        request(userInfoUrl, function(error, response, body){
-                            if (!error && response.statusCode == 200) {
-                                body = JSON.parse(body)
-                                console.log(body)
-                                res.send(body)
-                            }
-                        })
-                    }else{
-                        console.log('发生未知错误')
-                    }
-                })
-    //         }
-    //     }
-    // })
-
-
+            //拉取用户信息(需scope为 snsapi_userinfo)
+            /**
+             * 请求方法
+             * http：GET（请使用https协议） https://api.weixin.qq.com/sns/userinfo?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN
+             */
+            var userInfoUrl = 'https://api.weixin.qq.com/sns/userinfo?access_token='+access_token+'&openid='+openid+'&lang=zh_CN'
+            request(userInfoUrl, function(error, response, body){
+                if (!error && response.statusCode == 200) {
+                    body = JSON.parse(body)
+                    console.log(body)
+                    res.send(body)
+                }
+            })
+        }else{
+            console.log('发生未知错误')
+        }
+    })
 })
 
 router.get('/user_answer', function(req, res, next){
@@ -102,17 +100,18 @@ router.get('/user_answer', function(req, res, next){
      */
     //接收到用户请求，查询用户是否存在
     var mysql = require('mysql');
+
     var connection = mysql.createConnection({
-      host     : 'qdm130083629.my3w.com',  //121.42.100.211
-      port: 3306,
-      user     : 'qdm130083629',
-      password : 'yang1996',
-      database : 'qdm130083629_db',
-      insecureAuth: true
+        host     : '47.92.148.1',
+        port: 3306,
+        user     : 'design',
+        password : 'hHMTDMwskSSsjzcM',
+        database : 'design',
+        insecureAuth: true
     });
 
     connection.connect();
-    var sql = 'SELECT * FROM user where openId='+'"'+req.query.openId+'"'
+    var sql = 'SELECT * FROM weixindb where openId='+'"'+req.query.openId+'"'
     connection.query(sql, function(err, result) {
         if(err){
             console.log('[SELECT ERROR] - ',err.message);
@@ -121,7 +120,7 @@ router.get('/user_answer', function(req, res, next){
         console.log('--------------------------SELECT----------------------------');
         //当没有该用户时添加用户
         if(result.length === 0){
-            var  addSql = 'INSERT INTO user(id,nickname,openId,history) VALUES(0,?,?,?)';
+            var  addSql = 'INSERT INTO weixindb(id,nickname,openId,history) VALUES(0,?,?,?)';
             var history = []
             history.push({
                 qId: req.query.qId,
@@ -168,7 +167,7 @@ router.get('/user_answer', function(req, res, next){
                     ansTime: new Date().toLocaleString()
                 })
                 //没答过该题
-                var modSql = 'UPDATE user SET history = ? WHERE openId='+'"'+req.query.openId+'"';
+                var modSql = 'UPDATE weixindb SET history = ? WHERE openId='+'"'+req.query.openId+'"';
                 var modSqlParams = [JSON.stringify(history)];
 
                 //改
@@ -197,20 +196,22 @@ router.get('/user_answer', function(req, res, next){
 
 router.get('/user_history', function(req, res){
     var openId = req.query.openId
+
     var mysql      = require('mysql');
+
     var connection = mysql.createConnection({
-      host     : 'qdm130083629.my3w.com',  //121.42.100.211
-      port: 3306,
-      user     : 'qdm130083629',
-      password : 'yang1996',
-      database : 'qdm130083629_db',
-      insecureAuth: true
+        host     : '47.92.148.1',
+        port: 3306,
+        user     : 'design',
+        password : 'hHMTDMwskSSsjzcM',
+        database : 'design',
+        insecureAuth: true
     });
 
     connection.connect();
 
     console.log(req.query)
-    var sql = 'SELECT * FROM user where openId='+'"'+req.query.openId+'"'
+    var sql = 'SELECT * FROM weixindb where openId='+'"'+req.query.openId+'"'
     //查
     connection.query(sql,function (err, result) {
         if(err){
@@ -242,7 +243,7 @@ module.exports = router;
 
 // router.get('/api/login', function(req, res, next) {
 //     console.log("接收到请求"+ req.query.phoneNumber);
-//     var username = req.query.phoneNumber;
+//     var weixindbname = req.query.phoneNumber;
 //     req.session.username = username;
 
 //     var data = {
